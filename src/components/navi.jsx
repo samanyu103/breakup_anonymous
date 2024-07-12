69ff852e02111565b90b6767356bd75bd60414fd
akshaysethi2272005 <sethisadhana26@gmail.com>
2024-07-12 19:00:00 +0530

    someupd

--- src/components/navi.jsx
+++ src/components/navi.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./navi.css";
 const Navi = () => {
+    const [i, seti] = useState(true);
   return (
     <nav className='nav'>
         <ul className="list">
             <li className="item">Bran</li>
             <li className="item">
-                <div className="hamburger">
-                    <div className="ham"></div>
-                    <div className="ham"></div>
-                    <div className="ham"></div>
+                <div className="hamburger" onClick={()=>{seti(!i)}}>
+                    <div className={i ? "ham" : "ham c1"}></div>
+                    <div className={i ? "ham" : "ham c2"}></div>
+                    <div className={i ? "ham" : "ham c3"}></div>
                 </div>
             </li>
         </ul>
-        <ul className="list">
+        <ul className={i ? "list" : "nbi"}>
             <li className="item">Home</li>
             <li className="item">About Us</li>
             <li className="item">Contact Us</li>
