533f595a8e6eac0885be7a77618a8bdb3236543b
samanyu103 <mahajansamanyu@gmail.com>
2024-07-19 11:52:41 +0530

    grid

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -3,12 +3,31 @@ import "./grid.css";
 const Grid = () => {
   return (
     <div className="fea-grid">
-        <div className="grid"></div>
-        <div className="grid"></div>
-        <div className="grid"></div>
-        <div className="grid"></div>
-        <div className="grid"></div>
-        <div className="grid"></div>
+      <div className="grid">
+          <span className="feature">Community</span>
+          <img src="/images/community.jpeg" alt="community" className="image" />
+      </div>
+      <div className="grid">
+          <span className="feature">Resources</span>
+          <img src="/images/resources.png" alt="resources" className="image" />
+      </div>
+      <div className="grid">
+          <span className="feature">Therapy</span>
+          <img src="/images/therapy.png" alt="therapy" className="image" />
+      </div>
+
+      <div className="grid">
+          <span className="feature">Activities</span>
+          <img src="/images/gym.jpeg" alt="activities" className="image" />
+      </div>
+      <div className="grid">
+          <span className="feature">Journaling</span>
+          <img src="/images/journaling.png" alt="journaling" className="image" />
+      </div>
+      <div className="grid">
+          <span className="feature">Spirituality</span>
+          <img src="/images/meditation.jpeg" alt="meditation" className="image" />
+      </div>
     </div>
   )
 }
